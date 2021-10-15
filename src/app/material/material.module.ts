712d4be9a574adef90a9692f1da2f56845f00ee8
Hema35 <cheershema@gmail.com>
2021-10-14 20:06:11 -0400

    Input created

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
-
+import { MatInputModule } from "@angular/material/input";
  const MaterialComponents = [MatButtonModule,
-MatIconModule];
+MatIconModule, MatInputModule];
 
 @NgModule({
   imports: [ MaterialComponents ],
