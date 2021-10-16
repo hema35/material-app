a98399814c279f2f6d9b99b18de0f66e7488d2d5
Hema35 <cheershema@gmail.com>
2021-10-15 20:11:40 -0400

    services added

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CountriesService } from 'src/app/services/countries.service';
-
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
@@ -13,9 +13,9 @@ formGroup: FormGroup;
   constructor(private countriesService:CountriesService) {
     this.formGroup = new FormGroup(
       {
-        email: new FormControl(null),
-        customerName: new FormControl(null),
-        country: new FormControl(null)
+        email: new FormControl(null,[Validators.required,Validators.email]),
+        customerName: new FormControl(null,[Validators.required,Validators.maxLength(30),Validators.pattern('^[A-Za-z]*$')]),
+        country: new FormControl(null,[Validators.required])
       }
     )
    }
@@ -28,6 +28,46 @@ formGroup: FormGroup;
     {
       console.log(error);
     });
+  }
+  getFormControl(controlName:string):FormControl{
+return this.formGroup.get(controlName) as FormControl;
+  }
+
+  getErrorMessage(controlName:string,errorType:string):string{
+  switch(controlName){
+    case "customerName":
+      {
+        if(errorType === "required")
+        return "you must specify ";
+        else if(errorType === "maxlength")
+        return "can contain upto 30 characters";
+        else if(errorType === "pattern")
+        return "can contain alphabets";
+        else
+        return "";
+      }
+
+      case "email":
+        {
+          if(errorType === "required")
+          return "you must specify ";
+          else if(errorType === "email")
+          return "<strong>Email</strong>should be in correct format";
+          else
+          return "";
+        }
+
+        case "country":
+          {
+            if(errorType === "required")
+            return "you must select ";
+
+            else
+            return "";
+          }
+
+          default: return"";
+  }
   }
 
 }
